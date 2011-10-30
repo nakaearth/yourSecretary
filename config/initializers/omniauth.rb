dea5e87184bd7cdd480bb779e0ee6fa2389bdef2
Nakamura shinichirou <naka5313@gmail.com>
2011-10-30 12:18:45 +0900

    update Gemfile

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'cxhImnAeGuuJDKESgvmqQ', 'lqIOiWjPxrJ1lpF7zOe6H0PwG1vlPX9G8RkGvUDAGI'
+  provider :twitter, 'HKSkVKAmsPNhAXEDmZP2w', '9lu5yNeGjFWdfaA8nrizmHmJMty86a7vLNLbq8Vh4'
 end 
